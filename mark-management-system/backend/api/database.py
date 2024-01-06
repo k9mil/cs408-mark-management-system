ad80b001ed54bd0bfd709c127f01bedb8e5d4d5b
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-01-06 13:55:49 +0000

    fix: change to database_url

--- mark-management-system/backend/api/database.py
+++ mark-management-system/backend/api/database.py
@@ -6,7 +6,7 @@ from api.config import DevelopmentConfig
 from api.config import TestingConfig
 
 
-database_url = DevelopmentConfig.DATABASE_URL or TestingConfig.TESTING_URL
+database_url = DevelopmentConfig.DATABASE_URL or TestingConfig.DATABASE_URL
 engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
