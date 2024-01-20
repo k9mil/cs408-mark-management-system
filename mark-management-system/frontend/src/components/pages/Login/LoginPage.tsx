031cd7a4940f5f907c0089437a0465a24832fc22
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-01-20 12:27:19 +0000

    wip: add input/label to signin page

--- mark-management-system/frontend/src/components/pages/Login/LoginPage.tsx
+++ mark-management-system/frontend/src/components/pages/Login/LoginPage.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+import { Label } from "@/components/common/Label";
+import { Input } from "@/components/common/Input";
+import { Button } from "@/components/common/Button";
+
 const LoginPage = () => {
   return (
     <div className="flex flex-row h-screen w-screen">
@@ -12,14 +16,26 @@ const LoginPage = () => {
         </h2>
       </div>
       <div className="w-1/2 flex justify-center items-center">
-        <div className="flex flex-col">
-          <div className="flex flex-col items-center space-y-4">
-            <h1 className="font-bold text-2xl">Welcome Back! Please Log In</h1>
-            <h2 className="text-gray-400">
-              Enter your email and password below to access your account
+        <div className="flex flex-col space-y-12">
+          <div className="flex flex-col items-center space-y-2">
+            <h1 className="font-semibold text-xl">
+              Welcome Back! Please Log In
+            </h1>
+            <h2 className="text-gray-400 text-sm font-light">
+              Enter your details below to access your account.
             </h2>
           </div>
-          <div></div>
+          <div className="flex flex-col space-y-4">
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" placeholder="name@example.com" type="email" />
+            </div>
+            <div>
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" placeholder="•••••••••" type="password" />
+            </div>
+            <Button>Sign In with Email</Button>
+          </div>
         </div>
       </div>
     </div>
