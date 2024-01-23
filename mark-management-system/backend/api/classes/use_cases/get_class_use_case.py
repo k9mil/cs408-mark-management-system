7fc0a05f1234b871b34e889bf5e24e7c22cbe41a
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-01-23 07:38:13 +0000

    feat: add get class endpoint

---
+++ mark-management-system/backend/api/classes/use_cases/get_class_use_case.py
@@ -0,0 +1,29 @@
+from typing import Tuple
+
+from api.system.schemas.schemas import Class as ClassSchema
+
+from api.classes.repositories.class_repository import ClassRepository
+from api.users.repositories.user_repository import UserRepository
+
+from api.classes.errors.class_not_found import ClassNotFound
+
+
+class GetClassUseCase:
+    def __init__(self, class_repository: ClassRepository, user_repository: UserRepository):
+        self.class_repository = class_repository
+        self.user_repository = user_repository
+    
+    def execute(self, class_id: int, current_user: Tuple[str, bool]) -> list[ClassSchema]:
+        user_email, is_admin = current_user
+
+        lecturer = self.user_repository.find_by_email(user_email)
+        
+        if is_admin is False or lecturer is None:
+            raise PermissionError("Permission denied to access this resource")
+        
+        class_ = self.class_repository.get_class(class_id)
+
+        if class_ is None:
+            raise ClassNotFound("Class not found")
+
+        return class_
