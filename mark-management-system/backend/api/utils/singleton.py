6261a5e80ba6496383269d74af8135ca3223062a
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-01-30 19:34:30 +0000

    feat: add more typehints & whitespace

--- mark-management-system/backend/api/utils/singleton.py
+++ mark-management-system/backend/api/utils/singleton.py
@@ -1,7 +1,12 @@
-def singleton(class_):
-    instances = {}
+from typing import Any, Dict, Callable
+
+
+def singleton(class_: Any) -> Callable:
+    instances: Dict = {}
+
     def getinstance(*args, **kwargs):
         if class_ not in instances:
             instances[class_] = class_(*args, **kwargs)
         return instances[class_]
+    
     return getinstance
