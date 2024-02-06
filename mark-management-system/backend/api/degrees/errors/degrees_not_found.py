571c4c2f92aba8c777fc5e55948c2541e6aa7640
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-02-06 19:59:59 +0000

    docs: add docstrings + types for degree errors

--- mark-management-system/backend/api/degrees/errors/degrees_not_found.py
+++ mark-management-system/backend/api/degrees/errors/degrees_not_found.py
@@ -1,3 +1,9 @@
 class DegreesNotFound(Exception):
-    def __init__(self, message):
+    """
+    A custom subclass exception, raised when degrees are not found.
+
+    Args:
+        message: A parameter which allows for a custom error message.
+    """
+    def __init__(self, message: str) -> None:
         self.message = message
