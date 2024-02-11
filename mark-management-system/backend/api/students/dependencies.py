c96a8c107e560f581847a73d1835c78d0b5a2620
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-02-11 08:27:19 +0000

    feat: add get all students endpoint

--- mark-management-system/backend/api/students/dependencies.py
+++ mark-management-system/backend/api/students/dependencies.py
@@ -5,6 +5,7 @@ from api.middleware.dependencies import UserRepository
 
 from api.students.use_cases.create_student_use_case import CreateStudentUseCase
 from api.students.use_cases.get_student_use_case import GetStudentUseCase
+from api.students.use_cases.get_students_use_case import GetStudentsUseCase
 
 from api.middleware.dependencies import get_student_repository
 from api.middleware.dependencies import get_user_repository
@@ -24,6 +25,15 @@ def get_student_use_case(
         user_repository: UserRepository = Depends(get_user_repository)
     ) -> GetStudentUseCase:
     return GetStudentUseCase(
+        student_repository,
+        user_repository, 
+    )
+
+def get_students_use_case(
+        student_repository: StudentRepository = Depends(get_student_repository),
+        user_repository: UserRepository = Depends(get_user_repository)
+    ) -> GetStudentsUseCase:
+    return GetStudentsUseCase(
         student_repository,
         user_repository, 
     )
