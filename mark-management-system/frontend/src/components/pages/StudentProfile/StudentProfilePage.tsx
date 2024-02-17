fef6cfad5e0dc633148f52bf1262838dfb949cef
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-02-17 09:36:25 +0000

    feat: changed empty state and removed personal circumstances for now

--- mark-management-system/frontend/src/components/pages/StudentProfile/StudentProfilePage.tsx
+++ mark-management-system/frontend/src/components/pages/StudentProfile/StudentProfilePage.tsx
@@ -116,16 +116,16 @@ const StudentProfilePage = () => {
   return (
     <div className="bg-primary-blue h-screen w-screen flex">
       <Sidebar />
-      <div className="w-4/5 h-[95vh] m-auto bg-slate-100 rounded-3xl overflow-hidden">
-        <div className="m-8 flex flex-row space-x-12 w-full h-full">
-          <div className="flex flex-col space-y-4 w-full h-full">
-            <div className="flex 2xl:flex-col xl:flex-row 2xl:space-y-1 xl:space-x-2 2xl:space-x-0 xl:items-center 2xl:items-start">
-              <h1 className="text-3xl font-bold">Student Profile</h1>
-              <h2 className="text-gray-400">
-                Enter a student name to view their history of uploaded marks
-              </h2>
-            </div>
-            <div className="flex flex-row justify-between py-2 w-1/5">
+      <div className="w-4/5 h-[95vh] m-auto bg-slate-100 rounded-3xl">
+        <div className="m-8 flex flex-col space-y-4">
+          <div className="flex 2xl:flex-col xl:flex-row 2xl:space-y-1 xl:space-x-2 2xl:space-x-0 xl:items-center 2xl:items-start">
+            <h1 className="text-3xl font-bold">Student Profile</h1>
+            <h2 className="text-gray-400">
+              Enter a student name to view their history of uploaded marks
+            </h2>
+          </div>
+          <div className="flex flex-row justify-between py-2 h-4/5">
+            <div className="flex items-center w-1/5">
               {studentList !== null && studentList.length !== 0 ? (
                 <StudentProfileDropdown
                   student={student}
@@ -136,69 +136,27 @@ const StudentProfilePage = () => {
                 />
               ) : null}
             </div>
-            {student && student !== "" ? (
-              <StudentProfileDataTable
-                columns={StudentColumns}
-                data={studentMarks}
-                accessToken={accessToken}
-              />
-            ) : (
-              <div className="flex flex-col justify-center items-center w-auto h-[70%] space-y-4">
-                <div className="w-96 h-96">
-                  <img src="/empty_state.svg" alt="No data found" />
-                </div>
-                <div className="space-y-2 flex flex-col justify-center items-center">
-                  <h1 className="font-bold text-2xl">No data found</h1>
-                  <h2 className="font-light text-md">
-                    This area will fill with information once a user has been
-                    picked.
-                  </h2>
-                </div>
-              </div>
-            )}
           </div>
           {student && student !== "" ? (
-            <div className="flex flex-col">
-              <h1 className="text-3xl font-bold mb-8">
-                Personal Circumstances
-              </h1>
-              <div className="space-y-12">
-                {studentPersonalCircumstances &&
-                studentPersonalCircumstances.length > 0
-                  ? studentPersonalCircumstances.map(
-                      (personalCircumstance: IPersonalCircumstance) => (
-                        <div className="flex flex-col">
-                          <div className="space-x-2 flex flex-row">
-                            <h2 className="font-sm font-bold">Details:</h2>
-                            <h2 className="font-sm w-full">
-                              {personalCircumstance.details}
-                            </h2>
-                          </div>
-                          <div className="space-x-2 flex flex-row">
-                            <h2 className="font-sm font-bold">Semester:</h2>
-                            <h2 className="font-sm">
-                              {personalCircumstance.semester}
-                            </h2>
-                          </div>
-                          <div className="space-x-2 flex flex-row">
-                            <h2 className="font-sm font-bold">Category:</h2>
-                            <h2 className="font-sm">
-                              {personalCircumstance.cat}
-                            </h2>
-                          </div>
-                          <div className="space-x-2 flex flex-row">
-                            <h2 className="font-sm font-bold">Comments:</h2>
-                            <h2 className="font-sm">
-                              {personalCircumstance.comments}
-                            </h2>
-                          </div>
-                        </div>
-                      )
-                    )
-                  : null}
+            <StudentProfileDataTable
+              columns={StudentColumns}
+              data={studentMarks}
+              accessToken={accessToken}
+            />
+          ) : (
+            <div className="flex flex-col justify-center items-center m-auto w-auto h-auto space-y-4">
+              <div className="w-96 h-96">
+                <img src="/empty_state.svg" alt="No data found" />
+              </div>
+              <div className="space-y-2 flex flex-col justify-center items-center">
+                <h1 className="font-bold text-2xl">No data found</h1>
+                <h2 className="font-light text-md">
+                  This area will fill with information once a user has been
+                  picked.
+                </h2>
               </div>
             </div>
-          ) : null}
+          )}
         </div>
       </div>
     </div>
