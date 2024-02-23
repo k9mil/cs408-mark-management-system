f4bef2d9a52dbb2dca9a39165c399757f8e16a03
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-02-23 14:51:04 +0000

    feat: add global statistics to admin view

--- mark-management-system/frontend/src/components/pages/Dashboard/AdministratorView.tsx
+++ mark-management-system/frontend/src/components/pages/Dashboard/AdministratorView.tsx
@@ -1,6 +1,8 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Bar } from "react-chartjs-2";
+
+import { useAuth } from "../../../AuthProvider";
 
 import {
   Chart as ChartJS,
@@ -18,37 +20,71 @@ import {
   CardDescription,
 } from "@/components/common/Card";
 
+import { markService } from "@/services/MarkService";
+import { IStatistics } from "@/models/IMark";
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
 
-const data = {
-  labels: ["0-19%", "20-39%", "40-59%", "60-79%", "80-100%"],
-  datasets: [
-    {
-      label: "Number of Students",
-      data: [10, 20, 30, 40, 25],
-      backgroundColor: [
-        "rgba(98, 178, 253, 1)",
-        "rgba(155, 223, 196, 1)",
-        "rgba(249, 155, 171, 1)",
-        "rgba(255, 180, 79, 1)",
-        "rgba(159, 151, 247, 1)",
-      ],
-      borderColor: [
-        "rgba(98, 178, 253, 1)",
-        "rgba(155, 223, 196, 1)",
-        "rgba(249, 155, 171, 1)",
-        "rgba(255, 180, 79, 1)",
-        "rgba(159, 151, 247, 1)",
-      ],
-    },
-  ],
-};
-
-const options = {
-  maintainAspectRatio: false,
-};
-
 const AdministratorView = () => {
+  const { getAccessToken } = useAuth();
+  const accessToken = getAccessToken();
+
+  const [globalStatistics, setGlobalStatistics] = useState<IStatistics>();
+
+  const data = {
+    labels: ["0-19%", "20-39%", "40-59%", "60-79%", "80-100%"],
+    datasets: [
+      {
+        label: "Number of Students",
+        data: globalStatistics
+          ? [
+              globalStatistics.first_bucket,
+              globalStatistics.second_bucket,
+              globalStatistics.third_bucket,
+              globalStatistics.fourth_bucket,
+              globalStatistics.fifth_bucket,
+            ]
+          : [0, 0, 0, 0, 0],
+        backgroundColor: [
+          "rgba(98, 178, 253, 1)",
+          "rgba(155, 223, 196, 1)",
+          "rgba(249, 155, 171, 1)",
+          "rgba(255, 180, 79, 1)",
+          "rgba(159, 151, 247, 1)",
+        ],
+        borderColor: [
+          "rgba(98, 178, 253, 1)",
+          "rgba(155, 223, 196, 1)",
+          "rgba(249, 155, 171, 1)",
+          "rgba(255, 180, 79, 1)",
+          "rgba(159, 151, 247, 1)",
+        ],
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+  };
+
+  const globalStatisticsData = async () => {
+    try {
+      if (accessToken) {
+        const result = await markService.getGlobalStatistics(accessToken);
+        setGlobalStatistics(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    document.title = "Mark Management System | Dashboard";
+
+    globalStatisticsData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="bg-white rounded-3xl flex justify-center items-center m-8 p-8 h-2/3 w-3/4">
       <div className="grid grid-cols-3 grid-rows-2 gap-4 h-full w-full">
@@ -61,23 +97,31 @@ const AdministratorView = () => {
           <div className="flex flex-col justify-center items-center space-y-8 mt-4">
             <CardContent className="flex flex-row justify-around space-x-12 p-0">
               <div className="flex flex-col pl-12 justify-center items-center w-28 text-center">
-                <h1 className="font-bold text-3xl text-primary-blue">10%</h1>
+                <h1 className="font-bold text-3xl text-primary-blue">
+                  {globalStatistics ? globalStatistics.pass_rate + "%" : null}
+                </h1>
                 <h1 className="text-xs">Pass Rate</h1>
               </div>
               <div className="border-r-[1px] border-l-[1px] border-gray-200"></div>
               <div className="flex flex-col pr-12 justify-center items-center w-28 text-center">
-                <h1 className="font-bold text-3xl text-primary-blue">10%</h1>
+                <h1 className="font-bold text-3xl text-primary-blue">
+                  {globalStatistics ? globalStatistics.mean + "%" : null}
+                </h1>
                 <h1 className="text-xs">Mean</h1>
               </div>
             </CardContent>
             <CardContent className="flex flex-row justify-around space-x-12 p-0">
               <div className="flex flex-col pl-12 justify-center items-center w-28 text-center">
-                <h1 className="font-bold text-3xl text-primary-blue">10%</h1>
+                <h1 className="font-bold text-3xl text-primary-blue">
+                  {globalStatistics ? globalStatistics.median + "%" : null}
+                </h1>
                 <h1 className="text-xs">Median</h1>
               </div>
               <div className="border-r-[1px] border-l-[1px] border-gray-200"></div>
               <div className="flex flex-col pr-12 justify-center items-center w-28 text-center">
-                <h1 className="font-bold text-3xl text-primary-blue">10%</h1>
+                <h1 className="font-bold text-3xl text-primary-blue">
+                  {globalStatistics ? globalStatistics.mode + "%" : null}
+                </h1>
                 <h1 className="text-xs">Mode</h1>
               </div>
             </CardContent>
