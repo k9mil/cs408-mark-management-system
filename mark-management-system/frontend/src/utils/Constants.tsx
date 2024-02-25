a70d586fc38e02f10dfa0353450bf41a397b6af5
Kamil Zak <kamil.zak.2021@uni.strath.ac.uk>
2024-02-25 11:21:39 +0000

    feat: add prod flag to docker-compose & update constants to have two API URLs

--- mark-management-system/frontend/src/utils/Constants.tsx
+++ mark-management-system/frontend/src/utils/Constants.tsx
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://localhost/api/v1";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "http://3.8.144.109/api/v1"
+    : "http://localhost:8080/api/v1";
